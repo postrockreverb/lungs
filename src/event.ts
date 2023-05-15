c4baec13d951ac527eaef82f3e55a713f3082525
Alexandr Yermak <ayermak@protonmail.ch>
2023-05-16 01:44:26 +0300

    check if serverside

--- src/event.ts
+++ src/event.ts
@@ -1,9 +1,11 @@
+import { isServerSide } from './helpers';
+
 export const langPackUpdateEventName = 'langpackupdate';
 
 const langPackUpdateEvent = new Event(langPackUpdateEventName);
 
 export const collectEvent = () => {
-  if (typeof window === 'undefined') {
+  if (isServerSide()) {
     return;
   }
 
