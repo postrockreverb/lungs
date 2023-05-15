d978c45f19f9a9abe16baacf2fe1503c89fecf45
Alexandr Yermak <ayermak@protonmail.ch>
2023-05-16 01:41:00 +0300

    check if serverside

--- src/event.ts
+++ src/event.ts
@@ -3,5 +3,9 @@ export const langPackUpdateEventName = 'langpackupdate';
 const langPackUpdateEvent = new Event(langPackUpdateEventName);
 
 export const collectEvent = () => {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
   dispatchEvent(langPackUpdateEvent);
 };
