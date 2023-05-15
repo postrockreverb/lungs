0cdd4038a9f147daa22a55e168ba7e2cc89bc85f
SelenZzz <58228495+SelenZzz@users.noreply.github.com>
2023-05-15 22:34:07 +0000

    Build: (192e716) remove cache

--- dist/helpers/cache.d.ts
+++
@@ -1,4 +0,0 @@
-import { LangPack } from '../types';
-export declare const saveCache: (langPack: LangPack) => void;
-export declare const getCachedHash: () => string | null;
-export declare const getCached: () => LangPack | null;
